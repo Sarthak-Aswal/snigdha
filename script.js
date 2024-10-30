1d68e230b21c77c98a532d2367e754ad22ab2a01
Sarthak-Aswal <sarthakaswalgit@gmail.com>
2024-10-31 02:20:43 +0530

    sorry

--- script.js
+++ script.js
@@ -1,33 +1,64 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const slider = document.getElementById("rangeSlider");
-    const icon = document.getElementById("sliderIcon");
+const content = [
+    "Sorry Snigdha!!!",
+    "Maafi dedo",
+    "Pleaseeee!!!",
+    "Pleaseeee Pleaseeeee!!!",
+    "对不起",
+    "我非常非常抱歉",
+    "Last Chance Pleaseeee",
+    "Please na yaar!!!",
+    "Yahi dosti yahi Pyaar???",
+    "Aisa karoge??",
+    "Dedo na pleaseeee!!!",
+    "请索取",
+    "Sorry na yaar",
+    "对不起宝贝",
+    "Forgive me!!",
+    "Please accept my apologies!!",
+    "Lo siento",
 
-    // Function to update the icon's position and image source
-    function updateIcon() {
-        const value = slider.value;
-        const min = slider.min ? slider.min : 0;
-        const max = slider.max ? slider.max : 100;
-        const percent = (value - min) / (max - min) * 100;
 
-        // Update the icon's position
-        const sliderWidth = slider.offsetWidth;
-        const iconWidth = icon.offsetWidth;
-        const offset = ((percent / 100) * sliderWidth - (iconWidth / 100));
-        icon.style.left = `${offset}px`;
+];
 
-        // Update the icon's image source based on the slider value
-        if (value < 40) {
-            icon.src = 'images/icons/poop.png';  // Image for values below 40
-        } else if (value < 80) {
-            icon.src = 'images/icons/heart.png';  // Image for values between 40 and 80
-        } else {
-            icon.src = 'images/icons/lips.png';  // Image for values above 80
-        }
-    }
+// Generate a random number between 1 and 16 for the GIF path
+let randomNumber = Math.floor(Math.random() * 15) + 1;
 
-    // Update the icon when the slider is moved
-    slider.addEventListener("input", updateIcon);
+// Generate a random index for the message (0 to 6)
+let randomMessageIndex = Math.floor(Math.random() * content.length);
 
-    // Initial position and icon
-    updateIcon();
+// Get the corresponding message
+let randomMessage = content[randomMessageIndex];
+
+// Set the GIF path
+let gif = `${randomNumber}.gif`;
+
+// Get the elements by their IDs
+const message = document.getElementById("message");
+const image = document.getElementById("gif");
+const yes = document.getElementById("yes");
+const no = document.getElementById("no");
+const option=document.getElementById("option");
+const maafi=document.getElementById("maafi");
+// Set the message and image source
+message.innerText = randomMessage;
+image.src = `/images/${gif}`;
+
+no.addEventListener("click", function() {
+    randomMessageIndex = Math.floor(Math.random() * content.length);
+
+// Get the corresponding message
+randomMessage = content[randomMessageIndex];
+randomNumber = Math.floor(Math.random() * 22) + 1;
+message.innerText = randomMessage;
+gif = `${randomNumber}.gif`;
+image.src = `/images/${gif}`;   
+});
+
+yes.addEventListener('click',()=>{
+message.innerText="Thanks a lot babe a kissi for you";
+image.src = `/images/kiss.gif`;
+option.style.display="none";
+maafi.style.display="none";
+image.style.height="450px";
+
 });
